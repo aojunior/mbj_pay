019ac0f38886007748bfa09a7df53454d5b75756
aojunior <a.junior@mbjinformatica.com.br>
2024-05-27 09:11:16 -0300

    ajuste preload linux

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -4,19 +4,20 @@ import Home from './page/Home/Index'
 import CreateAccount from './page/CreateAccount'
 import Dashboard from './page/Dashboard'
 import { Route, Routes } from 'react-router-dom'
-import { useEffect } from 'react'
+import { useCallback, useEffect } from 'react'
 
 function App(): JSX.Element {
-  function refreshAndStorageToken() {
+
+  const refreshAndStorageToken = useCallback(() => {
     console.log('refresh Token')
     window.api.tokenGenerator()
     window.api.accessToken(data => sessionStorage.setItem('token', data))      
-  }
+  }, [])
 
   useEffect(() => {
     refreshAndStorageToken()
     setInterval(() => refreshAndStorageToken(), 5000 * 60) // 5 min
-  }, [])
+  }, [refreshAndStorageToken])
 
   return (
     <div style={{width: '100vw', alignItems: 'center', display: 'flex', flexDirection: 'column',}}>
